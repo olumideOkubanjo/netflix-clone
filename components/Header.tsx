4e550a22419e362bf41098d7e32e35e38fb7f197
Olumide Okubanjo <olumide.okubanjo@gmail.com>
2022-12-03 21:12:31 -0700

    added user profile picture

--- components/Header.tsx
+++ components/Header.tsx
@@ -45,6 +45,11 @@ function Header() {
                 <SearchIcon className='sm hidden h-6 w-6 sm:inline' />
                 <p className='hidden lg:inline'>Kids</p>
                 <BellIcon className='h-6 w-6' />
+                <img
+                    src='https://rb.gy/g1pwyx'
+                    alt=''
+                    className='cursor-pointer rounded'
+                />
             </div>
         </header>
     );
