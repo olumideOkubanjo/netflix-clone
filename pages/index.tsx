ab6389fa1785a78c41ffc089ad7af15cc6059088
Olumide Okubanjo <olumide.okubanjo@gmail.com>
2022-12-05 03:23:40 -0700

    added scroll lock

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,10 @@ const Home = ({
     if (loading) return null;
 
     return (
-        <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
+        <div
+            className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+                showModal && '!h-screen overflow-hidden'
+            }`}>
             <Head>
                 <title>Home - Netflix</title>
                 <link
